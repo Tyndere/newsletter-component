469f55492308e49785d5fbf1a9785ede8c9f5f0a
Cayden <caydenlincke4@gmail.com>
2025-03-13 13:15:13 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+// rollup.config.js
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: pkg.unpkg,
+      format: 'umd',
+      name: 'NewsletterComponent',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      },
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      },
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top',
+      },
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+};
